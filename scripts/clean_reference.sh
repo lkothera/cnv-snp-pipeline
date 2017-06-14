1768d7d2cdbafa36626cf1e1a9ef102537c8fd52
John Phan <ktr2@cdc.gov>
2017-06-14 09:10:10 -0400

    update reference_clean and backup old assoc_variants files

--- scripts/clean_reference.sh
+++ scripts/clean_reference.sh
@@ -9,18 +9,19 @@ source $(dirname "$0")/pipeline.sh
 
 # remove files
 rm -f ${BASE_REFERENCE}/${REFERENCE}-CDS.gtf
-rm -f ${BASE_REFERENCE}/${REFERENCE}-combined.gtf
+rm -f ${BASE_REFERENCE}/${REFERENCE}-exons.gtf
+rm -f ${BASE_REFERENCE}/${REFERENCE}-combined_gatk.gtf
 rm -f ${BASE_REFERENCE}/vectorbase_files/${VBFASTA}.fai
-#rm -f ${BASE_REFERENCE}/gene_names_for_grep.txt
-rm -f ${BASE_REFERENCE}/probes_final.txt
-#rm -f ${BASE_REFERENCE}/probes_step1.txt
-rm -f ${BASE_REFERENCE}/probes_full_step1.txt
-#rm -f ${BASE_REFERENCE}/probes_step2.txt
-rm -f ${BASE_REFERENCE}/probes_full_step2.txt
-rm -f ${BASE_REFERENCE}/gene_coords.txt
-rm -f ${BASE_REFERENCE}/bwa/supercontigs.fa*
-rm -f ${BASE_REFERENCE}/bwa/supercontigs_combined.fa*
-rm -f ${BASE_REFERENCE}/bwa/supercontigs_combined_formatted.fa*
-rm -f ${BASE_REFERENCE}/bwa/supercontigs_combined_formatted.dict
+rm -f ${BASE_REFERENCE}/gene_names_for_grep.txt
+rm -f ${BASE_REFERENCE}/probes_final_conifer.txt
+rm -f ${BASE_REFERENCE}/probes_final_gatk.txt
+rm -f ${BASE_REFERENCE}/probes_step1_conifer.txt
+rm -f ${BASE_REFERENCE}/probes_step1_gatk.txt
+rm -f ${BASE_REFERENCE}/probes_step2_conifer.txt
+rm -f ${BASE_REFERENCE}/probes_step2_gatk.txt
+rm -f ${BASE_REFERENCE}/gene_coords_conifer.txt
+rm -f ${BASE_REFERENCE}/gene_coords_gatk.txt
+rm -f ${BASE_REFERENCE}/bwa/*
 rmdir ${BASE_REFERENCE}/bwa
-rm -f ${BASE_REFERENCE}/supercontigs_uniq.txt
+rm -f ${BASE_REFERENCE}/supercontigs_uniq_conifer.txt
+rm -f ${BASE_REFERENCE}/supercontigs_uniq_gatk.txt
