754867c4ba4fff61443079f05ccf4cfa1bbe33c9
John Phan <ktr2@cdc.gov>
2017-06-19 17:15:37 -0400

    add metrics to gatk_features

--- scripts/run_gatk_assoc.sh
+++ scripts/run_gatk_assoc.sh
@@ -36,5 +36,5 @@ mkdir -p ${BASE}/pipeline-runs/${RUN_NAME}/gatk_features
 
 # run feature assoc script
 #${BASE}/scripts/assoc_variants2.py --vcf ${BASE}/pipeline-runs/${RUN_NAME}/gatk/variants.snp.vcf --compare ${BASE}/samples/${SAMPLES}/${COMPARE} --column ${COLUMN} > ${BASE}/pipeline-runs/${RUN_NAME}/gatk_features/variants.snp_assoc_${NAME}.txt
-${BASE}/scripts/assoc_variants.py --vcf ${BASE}/pipeline-runs/${RUN_NAME}/gatk/variants.snp.vcf --compare ${BASE}/samples/${SAMPLES}/${COMPARE} --column ${COLUMN} --fasta ${BASE_REFERENCE}/bwa/supercontigs_combined_formatted_gatk.fa --gtf ${BASE_REFERENCE}/${REFERENCE}-combined_gatk.gtf > ${BASE}/pipeline-runs/${RUN_NAME}/gatk_features/variants.snp_assoc_${NAME}.txt
+${BASE}/scripts/assoc_variants_new_tests.py --vcf ${BASE}/pipeline-runs/${RUN_NAME}/gatk/variants.snp.vcf --compare ${BASE}/samples/${SAMPLES}/${COMPARE} --column ${COLUMN} --fasta ${BASE_REFERENCE}/bwa/supercontigs_combined_formatted_gatk.fa --gtf ${BASE_REFERENCE}/${REFERENCE}-combined_gatk.gtf --thresh 0.05 > ${BASE}/pipeline-runs/${RUN_NAME}/gatk_features/variants.snp_assoc_${NAME}.txt
 
